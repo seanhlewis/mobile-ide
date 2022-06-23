98d305595526af2c30c7464938a566993dd61510
seanhlewis <96705270+seanhlewis@users.noreply.github.com>
2022-06-22 19:28:37 -0500

    Initial commit

---
+++ MobileIDE/AutoLoad/Globals.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var enter = 0
+
+var debug = false
+
+
+var keyboard = false
+var settings = false
+var Sanim = 0.0
+
+
+enum CheckCode{
+	NOT_CHECKED, 
+	UNKNOWN_OPERAND, 
+	WRONG_PLACE, 
+	CORRECT, 
+}
